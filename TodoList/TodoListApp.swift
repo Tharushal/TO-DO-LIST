dc030d84cb516cc15204e4b9b692fb59231ec590
Tharusha <tharushalakshan3@gmail.com>
2024-06-16 14:24:50 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Tharusha Lakshan on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
