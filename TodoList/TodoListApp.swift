da4586e141abb9fe8b905604460463c109361552
Tharusha <tharushalakshan3@gmail.com>
2024-06-18 23:00:11 +0530

    1st commit

--- TodoList/TodoListApp.swift
+++ TodoList/TodoListApp.swift
@@ -5,13 +5,25 @@
 //  Created by Tharusha Lakshan on 6/16/24.
 //
 
+/*
+ MVVM Architecture
+ 
+ -Model - data points
+ -View - UI
+ -ViewModel - manages models for view
+ 
+ */
+
 import SwiftUI
 
 @main
 struct TodoListApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ListView()
+            }
+            
         }
     }
 }
